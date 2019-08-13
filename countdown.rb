a4665c75233b683bc0234b3e55dfaf8a2868771c
Emilia ☆ <emilia.mena26@gmail.com>
2019-08-13 21:34:15 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,5 @@
 
-def countdown(number)
+def countdown
   number = 10 
   while number >= 0 
    puts "#{number} SECOND(S)!"
@@ -7,4 +7,12 @@ def countdown(number)
    sleep 5
   return "HAPPY NEW YEAR!"
 end
+end
+
+def using_while
+  levitation_force = 6
+  while levitation_force < 10 
+    puts "Wingardium Leviosa"
+    levitation_force += 1
+  end
 end
