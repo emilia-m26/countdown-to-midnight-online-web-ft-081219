e97c75a97001f9dd88c4e62d1c88dd0a52723f0e
Emilia ☆ <emilia.mena26@gmail.com>
2019-08-13 21:34:51 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,11 +8,3 @@ def countdown
   return "HAPPY NEW YEAR!"
 end
 end
-
-def using_while
-  levitation_force = 6
-  while levitation_force < 10 
-    puts "Wingardium Leviosa"
-    levitation_force += 1
-  end
-end
